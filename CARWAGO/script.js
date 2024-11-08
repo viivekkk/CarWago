03259136afb9534be6806ccc6184e64fe5fbbd84
viivekkk <vivekkeshav10@gmail.com>
2024-11-08 15:38:50 +0530

    Add files via upload

---
+++ CARWAGO/script.js
@@ -0,0 +1,14 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (name && email && message) {
+      alert(`Thank you, ${name}! Your message has been sent.`);
+    } else {
+      alert('Please fill out all fields.');
+    }
+  });
+  
+  
